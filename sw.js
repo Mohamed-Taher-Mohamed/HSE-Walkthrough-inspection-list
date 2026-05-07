c9e4b896961199e38b9e5e67f416295baaf0ad66
Mohamed-Taher-Mohamed <87355260+Mohamed-Taher-Mohamed@users.noreply.github.com>
2026-05-07 22:33:31 +0200

    Upgrade service worker cache version and improve fetch handling

--- sw.js
+++ sw.js
@@ -1,32 +1,44 @@
-const CACHE = "hse-cache-v2";
-
+const CACHE = "hse-cache-v3";
 const FILES = [
   "./",
   "./index.html",
-  "./script.js",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png"
 ];
-
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(FILES))
-  );
   self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(FILES))
+  );
 });
-
 self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))
+      Promise.all(
+        keys.map(key => {
+          if(key !== CACHE){
+            return caches.delete(key);
+          }
+        })
+      )
     )
   );
   self.clients.claim();
 });
-
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request)
+      .then(res => {
+        return res || fetch(e.request)
+          .then(networkRes => {
+            caches.open(CACHE).then(cache => {
+              cache.put(e.request, networkRes.clone());
+            });
+            return networkRes;
+          })
+          .catch(() => caches.match("./index.html"));
+      })
   );
 });
