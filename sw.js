9ae55775fc81124ba51c33d17c2ccac3d339e754
Mohamed-Taher-Mohamed <87355260+Mohamed-Taher-Mohamed@users.noreply.github.com>
2026-05-08 15:23:06 +0200

    Update service worker cache version and structure

--- sw.js
+++ sw.js
@@ -1,12 +1,15 @@
-const CACHE="hse-cache-v50";
+const CACHE="hse-cache-v60";
+
 const STATIC_FILES=[
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./script.js",
-  "./iconn-192.png",
-  "./iconn-512.png",
-  "./favicon.png"];
+"./",
+"./index.html",
+"./manifest.json",
+"./script.js",
+"./iconn-192.png",
+"./iconn-512.png",
+"./favicon.png"
+];
+
 self.addEventListener("install",event=>{
 self.skipWaiting();
 event.waitUntil(
@@ -15,6 +18,7 @@ return cache.addAll(STATIC_FILES);
 })
 );
 });
+
 self.addEventListener("activate",event=>{
 event.waitUntil(
 caches.keys().then(keys=>{
@@ -28,21 +32,39 @@ return caches.delete(key);
 }).then(()=>self.clients.claim())
 );
 });
+
 self.addEventListener("fetch",event=>{
+
+if(event.request.method!=="GET"){
+return;
+}
+
 event.respondWith(
 caches.match(event.request).then(cached=>{
+
 if(cached){
 return cached;
 }
+
 return fetch(event.request).then(response=>{
+
+if(!response||response.status!==200){
+return response;
+}
+
 const clone=response.clone();
+
 caches.open(CACHE).then(cache=>{
 cache.put(event.request,clone);
 });
+
 return response;
+
 }).catch(()=>{
 return caches.match("./index.html");
 });
+
 })
 );
+
 });
